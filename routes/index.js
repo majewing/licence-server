91a4da4a0d4883d06844285a2a3f1a1f7b5b024f
zhangye <zhangye@mtt.com>
2023-12-28 20:11:47 +0800

    test2 1

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
+/**
+ * GET home page.
+ */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET home page. */
+/**
+ * GET licence.
+ */
 router.get('/licence', function(req, res, next) {
   res.json({message: 'allow'});
 });
